43312e44d8778c152509c560df7c6e1b93bc60e4
Etienne van Delden <eirvandelden@me.com>
2015-01-09 12:47:43 +0100

    Implement guilt free days calculation

--- app/controllers/calculate_controller.rb
+++ app/controllers/calculate_controller.rb
@@ -1,11 +1,19 @@
 class CalculateController < ApplicationController
 
-def index
-end
+  def index
+  end
 
-def days
-  d = params[:days].to_i
-  @result = d
-end
+  def guiltfree_days
+    d = params[:days].to_i
+    p = params[:probability].to_f
+
+    @result = bin_co(6, d) * p**d * (1-p)**(6-d)
+  end
+
+private
+
+  def bin_co n, r
+    (0...r).inject(1) do |m,i| (m * (n - i)) / (i + 1) end
+  end
 
 end
